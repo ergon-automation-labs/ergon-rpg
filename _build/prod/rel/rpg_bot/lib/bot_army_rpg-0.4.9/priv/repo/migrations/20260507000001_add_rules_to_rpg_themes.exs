be2bd06db68cc24b9444052f49a5dfc9c8ba60b0
Abby Malson <august.malson@gmail.com>
2026-05-11 23:36:44 -0600

    chore: ignore _build/ and deps/ artifacts

--- _build/prod/rel/rpg_bot/lib/bot_army_rpg-0.4.9/priv/repo/migrations/20260507000001_add_rules_to_rpg_themes.exs
+++
@@ -1,9 +0,0 @@
-defmodule BotArmyRpg.Repo.Migrations.AddRulesToRpgThemes do
-  use Ecto.Migration
-
-  def change do
-    alter table(:rpg_themes) do
-      add(:rules, :map, default: %{})
-    end
-  end
-end
