c6ffcf69b583f150f354f1680bfaa2ca57649b81
Abby Malson <august.malson@gmail.com>
2026-09-02 08:53:37 +0300

    chore: Bump version to 0.15.26 to pick up runtime v0.14.62

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyRpg.MixProject do
   def project do
     [
       app: :bot_army_rpg,
-      version: "0.15.25",
+      version: "0.15.26",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -27,7 +27,7 @@ defmodule BotArmyRpg.MixProject do
   defp deps do
     [
       {:bot_army_library_core, path: "../bot_army_library_core"},
-      {:bot_army_library_runtime, path: "../bot_army_library_runtime"},
+      {:bot_army_library_runtime, path: "../bot_army_library_runtime", override: true},
       {:bot_army_library_learning, path: "../bot_army_library_learning"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, "~> 0.17"},
